8eceafea60be2870c7d616005afc46998533978f
Janvo <jan.vanoverveldt@kdg.be>
2018-04-26 15:44:19 +0200

    Verwijderen ingevulde gegevens

--- src/main/java/kdg/bigdata/StreamTutorialFunctions.java
+++ src/main/java/kdg/bigdata/StreamTutorialFunctions.java
@@ -22,14 +22,7 @@ public class StreamTutorialFunctions {
     SparkConf conf;
     JavaStreamingContext ssc;
 
-    @SuppressWarnings("unchecked")
-    Function3<String, Optional<Integer>, State<Integer>, Tuple2<String, Integer>> mappingFunc =
-            (word, one, state) -> {
-                int sum = one.orElse(0) + (state.exists() ? state.get() : 0);
-                Tuple2<String, Integer> output = new Tuple2<>(word, sum);
-                state.update(sum);
-                return output;
-            };
+
 
     public StreamTutorialFunctions(boolean forceLocal, int batchDuration) {
         conf = new SparkConf().setAppName("SparkStreamingTutorial");
@@ -40,44 +33,14 @@ public class StreamTutorialFunctions {
             conf.setMaster("local[2]");
         }
         ssc = new JavaStreamingContext(conf, new Duration(batchDuration));
-        ssc.checkpoint(".");
     }
 
     public void streamWordCount(String host, String port, String output) {
-        JavaDStream<String> lines = ssc.socketTextStream("127.0.0.1", 4444);
-        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
-        JavaPairDStream<String, Integer> pairs = words.mapToPair(s -> new Tuple2<>(s, 1));
-        JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey((i1, i2) -> i1 + i2);
-        JavaDStream<String> unpairedWords = wordCounts.map(i -> i._2 + " : " + i._1);
-        unpairedWords.dstream().saveAsTextFiles(output, "txt");
-        ssc.start();
-        try {
-            ssc.awaitTermination();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
     }
 
     public void streamWordCountWithState(String host, int port, String op) throws Exception {
 
-        // Initial state RDD input to mapWithState
-        @SuppressWarnings("unchecked")
-        List<Tuple2<String, Integer>> tuples =
-                Arrays.asList(new Tuple2<>("hello", 1), new Tuple2<>("world", 1));
-        JavaPairRDD<String, Integer> initialRDD = ssc.sparkContext().parallelizePairs(tuples);
-
-        JavaDStream<String> lines = ssc.socketTextStream(host, port, StorageLevels.MEMORY_AND_DISK_SER_2);
-        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
-        JavaPairDStream<String, Integer> wordsDstream = words.mapToPair(s -> new Tuple2<>(s, 1));
-        JavaMapWithStateDStream<String, Integer, Integer, Tuple2<String, Integer>> stateDstream =
-                wordsDstream.mapWithState(StateSpec.function(mappingFunc).initialState(initialRDD));
-
-
-        //stateDstream.dstream().print(10);
-
-        stateDstream.dstream().saveAsTextFiles(op,"");
-        ssc.start();
-        ssc.awaitTermination();
 
     }
 
